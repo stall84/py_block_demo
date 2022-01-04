ef4a90f1306fe8b1918e105685be5a0d6f6fc7d7
Michael Stallings <stall84@gmail.com>
2022-01-04 17:29:24 -0500

    Added global keyword for a 'counter'

--- block_basic/blockchain.py
+++ block_basic/blockchain.py
@@ -1,10 +1,26 @@
-blockchain = [[1]]
+blockchain = []
+counter = 0
 
 
-def add_value(transaction_amount):
-    blockchain.append([blockchain[-1], transaction_amount])
+def get_user_input():
+    return float(input("Please enter transaction amount: "))
+
+
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+
+def add_value(transaction_amount, last_transaction=[1]):
+    global counter
+    counter += 1
+    blockchain.append([last_transaction, transaction_amount])
+    print(counter)
     print(blockchain)
 
 
-add_value(5)
-add_value(15)
+tx_amount = get_user_input()
+add_value(tx_amount)
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
