34bf9e9e3e3877bcc9ed8ae168b0831bdf91f516
Michael Stallings <stall84@gmail.com>
2022-01-04 21:39:55 -0500

    final version .. pretty decent little blockchain emulator

--- block_basic/blockchain.py
+++ block_basic/blockchain.py
@@ -8,12 +8,20 @@ def get_transaction_value():
 
 
 def get_user_choice():
-    user_input = int(input("Please choose: "))
-    return user_input if user_input else None
+    user_input = input("Input : ")
+    if user_input == "Q":
+        return 3
+    return int(user_input) if user_input else None
 
 
 def get_last_blockchain_value():
     return blockchain[-1]
+
+
+def print_blockchain_data():
+    for block in blockchain:
+        print("Block : ", block)
+    print(f"BlockChain Iteration {counter}: ", blockchain)
 
 
 def add_value(transaction_amount, last_transaction=[0]):
@@ -21,7 +29,6 @@ def add_value(transaction_amount, last_transaction=[0]):
     counter += 1
     blockchain.append([last_transaction, transaction_amount])
     # print(counter)
-    print(f"BlockChain Iteration {counter}: ", blockchain)
 
 
 tx_amount = get_transaction_value()
@@ -29,15 +36,22 @@ add_value(tx_amount)
 
 
 while True:
+    print("Please choose: ")
     print("1: Add a new transaction amount ")
     print("2: Output the blockchain blocks")
-
+    print("Q: Quit")
     user_choice = get_user_choice()
     if user_choice == 1:
         tx_amount = get_transaction_value()
         add_value(tx_amount, get_last_blockchain_value())
         continue
     elif user_choice == 2:
-        for block in blockchain:
-            print("Block : ", block)
+        print_blockchain_data()
+        continue
+    elif user_choice == 3:
         break
+    else:
+        print("Input was invalid, please pick a value from the list!")
+        continue
+
+print("Done!")
