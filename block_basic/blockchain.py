834fbf1abe6afea9ca0f143043f53a8cd5716440
Michael Stallings <stall84@gmail.com>
2022-01-05 09:46:32 -0500

    Refined and cleaned up the structure.. Now theres no need to hardcode the first value-taking

--- block_basic/blockchain.py
+++ block_basic/blockchain.py
@@ -9,16 +9,13 @@ def get_transaction_value():
 
 def get_user_choice():
     user_input = input("Input : ")
-    if user_input == "Q":
-        return 3
-    return int(user_input) if user_input else None
+    return user_input if user_input else None
 
 
 def get_last_blockchain_value():
-    # if blockchain[0]:
-    if len(blockchain):
-        return blockchain[-1]
-    return
+    if len(blockchain) < 1:
+        return None
+    return blockchain[-1]
 
 
 def print_blockchain_data():
@@ -27,31 +24,33 @@ def print_blockchain_data():
     print(f"BlockChain Iteration {counter}: ", blockchain)
 
 
-def add_value(transaction_amount, last_transaction=[0]):
+def add_value(transaction_amount, last_transaction):
     global counter
     counter += 1
+    if (
+        last_transaction == None
+    ):  # Handle initial case where the blockchain will be empty (no -1 to access). For now we'll start every blockchain with [1]
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
-    # print(counter)
-
-
-tx_amount = get_transaction_value()
-add_value(tx_amount)
 
 
 while True:
     print("Please choose: ")
     print("1: Add a new transaction amount ")
     print("2: Output the blockchain blocks")
-    print("Q: Quit")
+    print("q: Quit")
     user_choice = get_user_choice()
-    if user_choice == 1:
+    if user_choice == "1":
         tx_amount = get_transaction_value()
         add_value(tx_amount, get_last_blockchain_value())
         continue
-    elif user_choice == 2:
+    elif user_choice == "2":
         print_blockchain_data()
         continue
-    elif user_choice == 3:
+    elif user_choice == "h":
+        print("H H >>>")
+        break
+    elif user_choice == "q":
         break
     else:
         print("Input was invalid, please pick a value from the list!")
