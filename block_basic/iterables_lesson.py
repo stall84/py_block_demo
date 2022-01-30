07bd0414e038745888ec92fd1371b316591b6e98
Michael Stallings <stall84@gmail.com>
2022-01-30 13:28:57 -0500

    Iterables breakout sesh

---
+++ block_basic/iterables_lesson.py
@@ -0,0 +1,15 @@
+# 3 types of iterables in Python
+
+# List (Array)
+iterable = [
+    "milk",
+    "honey",
+    "milk",
+]  # mutable, ordered (order guaranteed), duplicates allowed
+
+set_list = {
+    "milk",
+    "honey",
+}  # mutable, un-ordered (order not guaranteed). Written with curly brackets. NO duplicates allowed
+
+ma_tuple = ("Milk", "Honey")  # Imutable, ordered, duplicates OK,
