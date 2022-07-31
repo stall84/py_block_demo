008eedcd040e094d43812e5e4865e20702d4d0ac
Michael Stallings <stall84@gmail.com>
2022-07-31 12:07:46 -0400

    Succesfully converted all of our former block (pure)-dictionaries into our new class-object block and can now serialize to json for saving and loading data via the __dict__ method

--- block_basic/hash_util.py
+++ block_basic/hash_util.py
@@ -7,10 +7,12 @@ def hash_string_256(string):
 
 
 def hash_block(last_block):
-    '''
+    """
     Hashes a block and returns a string representation of it.
-    Arguments: 
+    Arguments:
         :last_block: The block that should be hashed
-    '''
-    # return '-'.join([str(last_block[key]) for key in last_block])
-    return hash_string_256(json.dumps(last_block, sort_keys=True).encode())
+    """
+    # Since moving to a class-object Block instead of just a dictionary. We now have trouble serializing to JSON.
+    # To avoid this, lets use python's __dict__ (dunder-dict lol)
+    hashable_block = last_block.__dict__.copy()
+    return hash_string_256(json.dumps(hashable_block, sort_keys=True).encode())
